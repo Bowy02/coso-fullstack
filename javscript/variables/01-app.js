cb1b616d7953545d1c15366a9d108f2d5fbd4744
Bowy <confioenmi12@gmail.com>
2026-03-28 23:00:19 +0100

    Introducción a JS y proyecto

---
+++ javscript/variables/01-app.js
@@ -0,0 +1,30 @@
+
+// Inicializar una variable con un valor
+var producto = "Monitor 24 Pulgadas";
+console.log(producto);
+
+// Las variablesse puede reasignar
+producto = "Monitor de 19 Pulgadas";
+console.log(producto);
+
+// Javascript es un lenguaje de tipo dinamico, no se especifíca tipo de dato.
+producto = 20;
+console.log(producto);
+
+// Se puedden inicializar sin valor y asignarlo después
+var disponible;
+disponible = true;
+disponible = false;
+
+// Inicializar múltiples variables.
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// Como var
+let edad = 29;
+
+// No se puede reasignar
+const nombre = "Sundente"
+
+
